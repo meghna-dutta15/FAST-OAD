dbcea4a65f7f458122dc458ab3be05fe31ba9dda
adbeck2 <adbeck2@illinois.edu>
2023-06-08 17:25:05 +0200

    rayleigh working question mark

--- src/fastoad/model_base/scramjet_propulsion/rayleigh.py
+++ src/fastoad/model_base/scramjet_propulsion/rayleigh.py
@@ -7,7 +7,8 @@ def mach_2_calculation(Min, pin, rin, Tin, rf, vf, Tf, cpin, cpf, cpc, A, mdotf,
     Ttf = Tf + (vf**2)/(2*cpf)
     T_t_1 = Tin + (vin**2)/(2*cpin)
     q = mdotf*hf + mdotf*cpf*Ttf
-    T_t_2 = (q/cpf) + T_t_1
+    #T_t_2 = (q/cpf) + T_t_1
+    T_t_2 = (mdotin*cpin*T_t_1 + mdotf*hf  + mdotf*cpf*Ttf) / ((mdotin+mdotf)*cpc)
 
     def get_rayleigh_stag_temperature_ratio(mach_1, mach_2, gamma=1.4):
         '''Return Tt2/Tt1 for Rayleigh flow'''
