4f333788089659ea2d3977b4e250ee03454b9088
adbeck2 <adbeck2@illinois.edu>
2023-06-15 18:24:02 +0200

    plotting

--- src/fastoad/model_base/scramjet_propulsion/thrust_vs_mach.py
+++ src/fastoad/model_base/scramjet_propulsion/thrust_vs_mach.py
@@ -1,27 +1,33 @@
 from parametrize_scramjet import parametrize_scramjet
 import numpy as np
+import matplotlib.pyplot as plt
 
 #input_dict = {'M_freestream': 8, 'p_freestream':1090.16, 'r_freestream':0.0167207, 'T_freestream':227.130, 'theta_1':0.09806, 'gamma_inlet':1.401, 'theta_2':0.23117, 'theta_3':0.32923, 'R':287.05, 'r_fuel':42, 'v_fuel':209.87, 'T_fuel':150, 'cp_inlet':1006, 'cp_fuel':12820, 'cp_combustor':1287.5, 'ER':0.029, 'hf':120e6, 'gamma_combustor':1.2869, 'theta_outlet':0.30238, 'gamma_outlet':1.31, 'A':0.05393}
 #input_dict = {'M_freestream': 8, 'p_freestream':1090.16, 'r_freestream':0.0167207, 'T_freestream':227.130, 'theta_1':0.09806, 'gamma_inlet':1.401, 'theta_2':0.23117, 'theta_3':0.32923, 'R':287.05, 'r_fuel':42, 'v_fuel':209.87, 'T_fuel':150, 'cp_inlet':1006, 'cp_fuel':12820, 'cp_combustor':1287.5, 'ER':0.029, 'hf':120e6, 'gamma_combustor':1.2869, 'theta_outlet':0.30238, 'gamma_outlet':1.31, 'A':0.25}
 input_vals = {'M_freestream': 8, 'p_freestream':1090.16, 'r_freestream':0.0167207, 'T_freestream':227.130, 'gamma_inlet':1.401, 'R':287.05, 'r_fuel':42, 'v_fuel':209.87, 'T_fuel':150, 'cp_inlet':1006, 'cp_fuel':12820, 'cp_combustor':1287.5, 'ER':0.029, 'hf':120e6, 'gamma_combustor':1.2869, 'gamma_outlet':1.31, 'alpha':0}
 scale_factors = {'x_scale':1, 'y_scale':1}
 
-alpha_array = np.array([-0.01, 0, 0.01])
-mach_array = np.array([8.5, 9, 9.5])
+mach_array = np.linspace(8,12,num=100)
 
-data = parametrize_scramjet(alpha_array, mach_array, scale_factors, input_vals)
+data = parametrize_scramjet(scale_factors, input_vals, mach_array=mach_array)
 data2 = data.parametrize_mach()
-print(data2[0].engine_thrust)
-print(data2[1].engine_thrust)
-print(data2[2].engine_thrust)
-
-thrust_array = np.array([data2[0].engine_thrust, data2[1].engine_thrust, data2[2].engine_thrust])
-print(thrust_array)
 
 thrust_array2 = []
 
 for i in data2:
     thrust_array2.append(i.engine_thrust)
 
-print(thrust_array2)
+#print(thrust_array2)
+
+thrust_array2 = np.array(thrust_array2)
+
+
+plt.style.use('bmh')
+plt.xlabel('Freestream Mach', fontsize = 15)
+plt.ylabel('Net Thrust (kN)', fontsize = 15)
+plt.title('Thrust vs Mach', fontsize = 15)
+plt.plot(mach_array, thrust_array2/1000)
+plt.show()
+
+
 
