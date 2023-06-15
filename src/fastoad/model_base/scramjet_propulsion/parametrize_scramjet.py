00dfd265792f1553b902a66a610155b2aee46113
adbeck2 <adbeck2@illinois.edu>
2023-06-15 15:42:57 +0200

    parametrized scale

--- src/fastoad/model_base/scramjet_propulsion/parametrize_scramjet.py
+++ src/fastoad/model_base/scramjet_propulsion/parametrize_scramjet.py
@@ -1,11 +1,13 @@
 from scramjet import scramjet
 
 class parametrize_scramjet():
-    def __init__(self, alpha_array, mach_array, scale_factors_nominal, input_vals_nominal):
+    def __init__(self, scale_factors_nominal, input_vals_nominal, alpha_array=None, mach_array=None, x_scale_array = None, y_scale_array=None):
         self.alpha_array = alpha_array
         self.mach_array = mach_array
         self.scale_factors = scale_factors_nominal
         self.input_vals = input_vals_nominal
+        self.x_scale_array = x_scale_array
+        self.y_scale_array = y_scale_array
     
     def parametrize_alpha(self):
         data_array = []
@@ -26,5 +28,24 @@ class parametrize_scramjet():
             data = scramjet(self.scale_factors, self.input_vals)
             data.run()
             data_array.append(data)
+
+        return data_array
+    
+
+    def parametrize_scale_factors(self):
+        data_array = []
+
+        for i in range(len(self.x_scale_array)):
+            self.scale_factors['x_scale'] = self.x_scale_array[i]
+            temp = []
+            for i in range(len(self.y_scale_array)):
+                self.scale_factors['y_scale'] = self.y_scale_array[i]
+            
+                data = scramjet(self.scale_factors, self.input_vals)
+                data.run()
+                temp.append(data)
+
+
+            data_array.append(temp)
 
         return data_array
---
+++ src/fastoad/model_base/scramjet_propulsion/test4.py
@@ -0,0 +1,23 @@
+from parametrize_scramjet import parametrize_scramjet
+import numpy as np
+
+#input_dict = {'M_freestream': 8, 'p_freestream':1090.16, 'r_freestream':0.0167207, 'T_freestream':227.130, 'theta_1':0.09806, 'gamma_inlet':1.401, 'theta_2':0.23117, 'theta_3':0.32923, 'R':287.05, 'r_fuel':42, 'v_fuel':209.87, 'T_fuel':150, 'cp_inlet':1006, 'cp_fuel':12820, 'cp_combustor':1287.5, 'ER':0.029, 'hf':120e6, 'gamma_combustor':1.2869, 'theta_outlet':0.30238, 'gamma_outlet':1.31, 'A':0.05393}
+#input_dict = {'M_freestream': 8, 'p_freestream':1090.16, 'r_freestream':0.0167207, 'T_freestream':227.130, 'theta_1':0.09806, 'gamma_inlet':1.401, 'theta_2':0.23117, 'theta_3':0.32923, 'R':287.05, 'r_fuel':42, 'v_fuel':209.87, 'T_fuel':150, 'cp_inlet':1006, 'cp_fuel':12820, 'cp_combustor':1287.5, 'ER':0.029, 'hf':120e6, 'gamma_combustor':1.2869, 'theta_outlet':0.30238, 'gamma_outlet':1.31, 'A':0.25}
+input_vals = {'M_freestream': 8, 'p_freestream':1090.16, 'r_freestream':0.0167207, 'T_freestream':227.130, 'gamma_inlet':1.401, 'R':287.05, 'r_fuel':42, 'v_fuel':209.87, 'T_fuel':150, 'cp_inlet':1006, 'cp_fuel':12820, 'cp_combustor':1287.5, 'ER':0.029, 'hf':120e6, 'gamma_combustor':1.2869, 'gamma_outlet':1.31, 'alpha':0}
+scale_factors = {'x_scale':1, 'y_scale':1}
+
+alpha_array = np.array([-0.01, 0, 0.01])
+mach_array = np.array([8.5, 9, 9.5])
+x_scale_array = np.array([0.95,1,1.05])
+y_scale_array = np.array([0.95,1,1.05])
+
+data = parametrize_scramjet(scale_factors, input_vals, x_scale_array=x_scale_array, y_scale_array=y_scale_array)
+data2 = data.parametrize_scale_factors()
+print(data2[0][2].engine_thrust)
+print(data2[1][2].engine_thrust)
+print(data2[2][2].engine_thrust)
+
+
+
+
+
